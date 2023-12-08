b73bc9903c65e9ce88ed002dfbbea0c56946d50b
1-nabe <rolf_thorben@web.de>
2023-12-08 20:38:49 +0100

    Solved day 8 part 1 + 2

---
+++ day8/solution.py
@@ -0,0 +1,29 @@
+# https://adventofcode.com/2023/day/8
+
+import time
+import re
+
+if __name__ == "__main__":
+    begin = time.perf_counter()
+    f = open('input.txt')
+    lines = f.readlines()
+
+    instructions = lines.pop(0).strip()
+    mapped_instructions = [1 if char == 'R' else 0 for char in instructions]
+    lines.pop(0)
+
+    nodes = {}
+    for i, line in enumerate(lines):
+        matches = re.findall(r'\b[a-zA-Z]{3}\b', line)
+        nodes[matches[0]] = [matches[1], matches[2]]
+
+    curr = 'AAA'
+    end = 'ZZZ'
+    steps = 0
+    while curr != end:
+        for direction in mapped_instructions:
+            curr = nodes[curr][direction]
+            steps += 1
+
+    print(f"Steps: {steps}") # Steps: 12361
+    print(f"Finished in: {time.perf_counter() - begin:.6f}s") # Finished in: 0.002538s
---
+++ day8/solution_2.py
@@ -0,0 +1,38 @@
+# https://adventofcode.com/2023/day/8
+
+import time
+import re
+import math
+import numpy as np
+
+if __name__ == "__main__":
+    begin = time.perf_counter()
+    f = open('input.txt')
+    lines = f.readlines()
+
+    instructions = lines.pop(0).strip()
+    mapped_instructions = [1 if char == 'R' else 0 for char in instructions]
+    lines.pop(0)
+
+    nodes = {}
+    start_nodes = []
+    for i, line in enumerate(lines):
+        matches = re.findall(r'\b[a-zA-Z]{3}\b', line)
+        nodes[matches[0]] = [matches[1], matches[2]]
+        if matches[0][2] == 'A':
+            start_nodes.append(matches[0])
+
+    print(start_nodes)
+
+    all_steps = []
+    for curr in start_nodes:
+        steps = 0
+        while curr[2] != 'Z':
+            for direction in mapped_instructions:
+                curr = nodes[curr][direction]
+                steps += 1
+        all_steps.append(steps)
+
+    
+    print(f"Steps: {np.lcm.reduce(all_steps)}") # Steps: 18215611419223
+    print(f"Finished in: {time.perf_counter() - begin:.6f}s") # Finished in: 0.013985s
