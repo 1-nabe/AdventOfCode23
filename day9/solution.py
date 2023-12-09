d42415e70d8c9369fa423f7b2e1dd7c420351dab
1-nabe <rolf_thorben@web.de>
2023-12-09 11:39:30 +0100

    Solved day 9 part 2

--- day9/solution.py
+++ day9/solution.py
@@ -2,28 +2,25 @@
 
 import time
 
-if __name__ == "__main__":
-    begin = time.perf_counter()
-    f = open('input.txt')
-    lines = f.readlines()
-
-    def calculate_difference(current_numbers):
+def calculate_difference(current_numbers):
         diff = []
         for i in range(1, len(current_numbers)):
             diff.append(current_numbers[i] - current_numbers[i - 1])
         return diff
     
+def extrapolate(lines):
     extrapolated_sum = 0
     for line in lines:
+        first_numbers = []
         last_numbers = []
         ended = False
 
-        line = line.strip()
-        current_numbers = [int(x) for x in line.split(' ')]
+        current_numbers = line
         while ended == False:
+            first_numbers.append(current_numbers[0])
             last_numbers.append(current_numbers[-1])
             current_numbers = calculate_difference(current_numbers)
-            
+
             unique_numbers = list(set(current_numbers))
             ended = len(unique_numbers) == 1 and unique_numbers[0] == 0
         
@@ -31,6 +28,14 @@ if __name__ == "__main__":
         for i in range(len(last_numbers)):
             extrapolated += last_numbers[i]
         extrapolated_sum += extrapolated
+    return extrapolated_sum
 
-    print(f"Extrapolated sum: {extrapolated_sum}") # Extrapolated sum: 1955513104
-    print(f"Finished in: {time.perf_counter() - begin:.6f}s") # Finished in: 0.004996s
+if __name__ == "__main__":
+    begin = time.perf_counter()
+    f = open('input.txt')
+    lines = f.readlines()
+    lines = [list(map(int, line.split())) for line in lines]
+    
+    print(f"Part 1 - Extrapolated future sum: {extrapolate(lines)}") # Extrapolated future sum: 1955513104
+    print(f"Part 2 - Extrapolated past sum: {extrapolate([list(reversed(line)) for line in lines])}") # Extrapolated past sum: 1131
+    print(f"Finished in: {time.perf_counter() - begin:.6f}s") # Finished in: 0.008111s
