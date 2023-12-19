9ecb503826a80d513847bbddcfff97bbbc38e649
1-nabe <rolf_thorben@web.de>
2023-12-19 20:52:20 +0100

    Solved day 18 part 2

--- day18/solution.py
+++ day18/solution.py
@@ -11,6 +11,22 @@ def parse_input(lines):
     return data_list
 
 
+def parse_color_code(color_code):
+    return parse_num_dir(color_code[7]), parse_hex(color_code[2:7])
+
+
+def parse_hex(hex):
+    return int(hex, 16)
+
+
+def parse_num_dir(num):
+    match int(num):
+        case 0: return 'R'
+        case 1: return 'D'
+        case 2: return 'L'
+        case 3: return 'U'
+
+
 def get_next_point(point, dir, dist):
     match dir:
         case 'R': return (point[0] + dist, point[1])
@@ -43,10 +59,18 @@ if __name__ == '__main__':
     begin = time.perf_counter()
     f = open(INPUT_PATH)
     parsed_data = parse_input(f.read())
-    cubic_meters = parse_points(parsed_data)
 
+    cubic_meters = parse_points(parsed_data)
     # Cubic meters of lava: 74074
-    print(f"Cubic meters of lava: {cubic_meters}")
+    print(f"Part 1 - Cubic meters of lava: {cubic_meters}")
 
     # Finished in: 0.000640s
+    print(f"Finished in: {time.perf_counter() - begin:.6f}s")
+
+    new_parsed_data = [parse_color_code(x[2]) for x in parsed_data]
+    cubic_meters = parse_points(new_parsed_data)
+    # Cubic meters of lava:
+    print(f"Part 2 - Cubic meters of lava: {cubic_meters}")
+
+    # Finished in:
     print(f"Finished in: {time.perf_counter() - begin:.6f}s")
